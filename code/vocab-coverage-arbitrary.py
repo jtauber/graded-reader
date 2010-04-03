2700926e9892d5d897da051e561fa4ac79406ecd
James Tauber <jtauber@jtauber.com>
2010-04-02 22:31:31 -0400

    tweaked comments and wrapping

--- code/vocab-coverage-arbitrary.py
+++ code/vocab-coverage-arbitrary.py
@@ -33,13 +33,15 @@ import sys
 TARGET_ITEM_FILENAME = sys.argv[1]
 LEARNING_PROGRAMME_FILENAME = sys.argv[2]
 
-# target_item_list: list of (target, item) tuples to save us loading the file twice
+# target_item_list: list of (target, item) tuples to save us loading the file
+# twice
 
 target_item_list = []
 for line in open(TARGET_ITEM_FILENAME):
     target_item_list.append(line.strip().split())
 
-# items: map of item to learning order, as indicated by loaded learning programme
+# items: map of item to learning order, as indicated by loaded learning
+# programme
 
 items = {}
 count = 1
@@ -66,9 +68,11 @@ for target in targets:
     targets[target] = sorted(targets[target])
 
 # so now if targets[X] = [5, 5, 20, 50],
-# it means that target X consists of the 5th learnt item (twice), the 20th learnt and the 50th learnt
-# it also means if you want to read 50% of this target (i.e. 2/4) you need to know up to the 5th learnt word
-# and if you want to read 75% of this target (i.e. 3/4) you need to know up to the 20th most learnt word
+# it means that target X consists of the 5th learnt item (twice), the 20th
+# learnt and the 50th learnt it also means if you want to read 50% of this
+# target (i.e. 2/4) you need to know up to the 5th learnt word and if you want
+# to read 75% of this target (i.e. 3/4) you need to know up to the 20th most
+# learnt word
 
 
 ## math helper functions
@@ -84,17 +88,18 @@ def percentage(n, d):
 
 ## calculate what's needed for each target
 
-# needed - maps a given coverage ratio to a list which, for each target, gives the last learnt word number 
-# necessary to reach that coverage ratio for that target
+# needed - maps a given coverage ratio to a list which, for each target, gives
+# the last learnt word number necessary to reach that coverage ratio for that
+# target
 needed = {}
 
 for coverage in COVERAGE:
     needed[coverage] = [targets[target][ceiling(coverage * len(targets[target])) - 1] for target in targets]
 
 # in other words if needed[0.50] = [16, 44, 182, 34, 21, 36, 8, 48, 21, 26],
-# that means that to achieve 50% coverage, the first target needs up to the 16th learnt word,
-# the second target needs up to the 44th learnt word, the third target needs up to the 182nd learnt word,
-# and so on...
+# that means that to achieve 50% coverage, the first target needs up to the
+# 16th learnt word, the second target needs up to the 44th learnt word, the
+# third target needs up to the 182nd learnt word, and so on...
 
 
 ## display table
@@ -107,8 +112,8 @@ print
 for item_count in ITEM_COUNTS:
     print item_count,
     for coverage in COVERAGE:
-        # how many targets require less than or equal to item_count to reach the given coverage?
+        # how many targets require less than or equal to item_count to reach
+        # the given coverage?
         num = len([freq for freq in needed[coverage] if freq <= item_count])
         print "\t%s%%" % (percentage(num, len(targets))),
     print
-    
--- code/vocab-coverage.py
+++ code/vocab-coverage.py
@@ -29,7 +29,8 @@ ITEM_COUNTS = [100, 200, 500, 1000, 2000, 5000]
 import sys
 FILENAME = sys.argv[1]
 
-# target_item_list: list of (target, item) tuples to save us loading the file twice
+# target_item_list: list of (target, item) tuples to save us loading the file
+# twice
 
 target_item_list = []
 for line in open(FILENAME):
@@ -67,9 +68,11 @@ for target in targets:
     targets[target] = sorted(targets[target])
 
 # so now if targets[X] = [5, 5, 20, 50],
-# it means that target X consists of the 5th most frequent item (twice), the 20th most frequent and the 50th most frequent
-# it also means if you want to read 50% of this target (i.e. 2/4) you need to know up to the 5th most frequent word
-# and if you want to read 75% of this target (i.e. 3/4) you need to know up to the 20th most frequenct word
+# it means that target X consists of the 5th most frequent item (twice), the
+# 20th most frequent and the 50th most frequent it also means if you want to
+# read 50% of this target (i.e. 2/4) you need to know up to the 5th most
+# frequent word and if you want to read 75% of this target (i.e. 3/4) you need
+# to know up to the 20th most frequenct word
 
 
 ## math helper functions
@@ -85,16 +88,18 @@ def percentage(n, d):
 
 ## calculate what's needed for each target
 
-# needed - maps a given coverage ratio to a list which, for each target, gives the least frequent word necessary to reach
-# that coverage ratio for that target
+# needed - maps a given coverage ratio to a list which, for each target, gives
+# the least frequent word necessary to reach that coverage ratio for that
+# target
 needed = {}
 
 for coverage in COVERAGE:
     needed[coverage] = [targets[target][ceiling(coverage * len(targets[target])) - 1] for target in targets]
 
 # in other words if needed[0.50] = [16, 44, 182, 34, 21, 36, 8, 48, 21, 26],
-# that means that to achieve 50% coverage, the first target needs up to the 16th most frequent word,
-# the second target needs up to the 44th most frequent word, the third target needs up to the 182nd most frequent word,
+# that means that to achieve 50% coverage, the first target needs up to the
+# 16th most frequent word, the second target needs up to the 44th most
+# frequent word, the third target needs up to the 182nd most frequent word,
 # and so on...
 
 
@@ -108,7 +113,8 @@ print
 for item_count in ITEM_COUNTS:
     print item_count,
     for coverage in COVERAGE:
-        # how many targets require less than or equal to item_count to reach the given coverage?
+        # how many targets require less than or equal to item_count to reach
+        # the given coverage?
         num = len([freq for freq in needed[coverage] if freq <= item_count])
         print "\t%s%%" % (percentage(num, len(targets))),
     print
